75a855ad1e05c75bf21bb7fb9a098e07ea7788cc
E-Okelloh <eouma359@gmail.com>
2026-03-28 00:00:59 +0300

    mutability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5; //immutable - this is a promise to the compiler
+    let mut y = 10; //mutable
+
+    //x = 6;
+    y = 10; // correct : y was declared mut
+
+    println!("x={}, y={}", x, y);
 }
